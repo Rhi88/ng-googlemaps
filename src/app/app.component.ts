c7f0529ba08e390513bc4a460f5c298439577fd2
Rhi88 <rhizdo_nop@hotmail.com>
2018-08-01 09:23:44 -0500

    Iconos en googlemaps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  icons = {
+    info: {
+      icon: 'http://icons.iconarchive.com/icons/google/noto-emoji-objects/32/62821-battery-icon.png'
+    }
+  };
+
+  title: string ='My first AGM project';
+  lat: number = 19.4226264;
+  lng: number = -99.1563623;
+
+  zoom: number = 12;
+
+markers: marker[] = [
+  {
+    lat: 19.4226,
+    lng: -99.156423,
+    label: 'E',
+    type: 'info',
+    draggable: false
+   
+  },
+  {
+    lat: 19.4226,
+    lng: -99.156499,
+    label : 'C',
+    type: 'info',
+    draggable: false
+    
+  },
+  {
+    lat: 19.4226250,
+    lng: -99.166455,
+    label :'B',
+    type: 'info',
+    draggable: false
+   
+  }
+]
+
+}
+// just an interface for type safety.
+interface marker {
+lat: number;
+lng: number;
+label?: string;
+draggable: boolean;
+type: string;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey:''
+    })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
